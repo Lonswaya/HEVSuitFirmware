3af09ef97ae765e3be8f7fb802d0953c41e079d2
Andrew Lonsway <alonzberry@gmail.com>
2021-08-22 16:53:48 -0400

    Selection logic

--- HEV/main.cpp
+++ HEV/main.cpp
@@ -13,12 +13,11 @@
 #define PLAY_3_LED 14
 #define PLAY_4_LED 15
 
+constexpr auto CHANNEL_MAX = 5;
+
 int main(void)
 {
 	wiringPiSetupSys();
-	//pinMode(CHANNEL_TOGGLE_LED, OUTPUT);
-	//pinMode(PLAY_1_LED, OUTPUT);
-
 
 	pinMode(CHANNEL_TOGGLE_LED, INPUT);
 	pinMode(PLAY_1_LED, INPUT);
@@ -26,25 +25,62 @@ int main(void)
 	pinMode(PLAY_3_LED, INPUT);
 	pinMode(PLAY_4_LED, INPUT);
 
-	// Play init audio
-	int count = 0;
+	// Play init audio TODO
+
+	int channel = 0;
+	bool playing = false;
+
+	int lastChannelToggle;
+	int lastPlay1;
+	int lastPlay2;
+	int lastPlay3;
+	int lastPlay4;
+
 	while (true)
 	{
-		/*delay(500); 
-		digitalWrite(CHANNEL_TOGGLE_LED, HIGH);
-		digitalWrite(PLAY_1_LED, HIGH);
-		delay(500);
-		digitalWrite(CHANNEL_TOGGLE_LED, LOW);
-		digitalWrite(PLAY_1_LED, LOW);
-		printf("cycle\n");*/
-		
 		int channelToggle = digitalRead(CHANNEL_TOGGLE_LED);
 		int play1 = digitalRead(PLAY_1_LED);
 		int play2 = digitalRead(PLAY_2_LED);
 		int play3 = digitalRead(PLAY_3_LED);
 		int play4 = digitalRead(PLAY_4_LED);
-		printf("Inputs cycle %d: %d %d %d %d %d\n", ++count, channelToggle, play1, play2, play3, play4);
-		delay(250);
+		//printf("Inputs cycle %d: %d %d %d %d %d\n", ++count, channelToggle, play1, play2, play3, play4);
+		delay(100);
+
+		if (channelToggle && channelToggle != lastChannelToggle)
+		{
+			channel = (channel + 1)% CHANNEL_MAX;
+			printf("Channel toggle: %d\n", channel);
+
+		}
+		if (play1 && play1 != lastPlay1)
+		{
+			// TODO play
+			printf("play1\n");
+		}
+
+		if (play2 && play2 != lastPlay2)
+		{
+			// TODO play
+			printf("play2\n");
+		}
+
+		if (play3 && play3 != lastPlay3)
+		{
+			// TODO play
+			printf("play3\n");
+		}
+
+		if (play4 && play4 != lastPlay4)
+		{
+			// TODO play
+			printf("play4\n");
+		}
+
+		lastChannelToggle = channelToggle;
+		lastPlay1 = play1;
+		lastPlay2 = play2;
+		lastPlay3 = play3;
+		lastPlay4 = play4;
 	}
 	return 0;
 }
